85b6ea3233d8eb102df168a42f2ccb722a85b002
Adam Zentest <email@address.com>
2017-06-20 05:15:30 -0400

    form formatting

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,9 @@
-class Company < ApplicationRecord
+class Company < ActiveRecord::Base
 
 def self.import(file)
 	CSV.foreach(file.path, header: true) do |row|
 		Company.create! row.to_hash
-end
+	end
 end
 
 end
